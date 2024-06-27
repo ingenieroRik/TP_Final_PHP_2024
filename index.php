266f8b51d5c322e8f5e228e24ba74d2c71184411
rik <ingeniero_rik@hotmail.com>
2024-06-27 00:55:17 -0300

    se elimina todo todo

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-require 'vendor/autoload.php'; // Asegúrate de que la ruta es correcta
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-
-
-include './src/controller/peliculasController.php';
+echo ("Hola Mundo");
 
 
 
