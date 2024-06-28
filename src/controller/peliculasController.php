fff513321d9511fd42d1e6bb340b7493f6b8a82c
rik <ingeniero_rik@hotmail.com>
2024-06-28 00:37:31 -0300

    configuro cors otra vez

--- src/controller/peliculasController.php
+++ src/controller/peliculasController.php
@@ -9,7 +9,7 @@ if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed
 // Manejo de solicitudes OPTIONS
 
 
-header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
