8ff76c2b334f9d3d8536d18555ade10965cb702d
rik <ingeniero_rik@hotmail.com>
2024-06-29 19:51:01 -0300

    modif index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-require 'vendor/autoload.php'; // Asegúrate de que la ruta es correcta
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
 
 include './src/controller/peliculasController.php';
