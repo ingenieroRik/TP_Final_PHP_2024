eaf90bd6df04693314782ee81eeeb2f7bd4484b3
rik <ingeniero_rik@hotmail.com>
2024-06-29 22:38:07 -0300

    modif l147 peli contr

--- src/controller/peliculasController.php
+++ src/controller/peliculasController.php
@@ -144,7 +144,7 @@ $PeliculasModel= new peliculasModel();
            en le POST verificamos que no sea _method=PUT */
        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['_method']) && strtoupper($_POST['_method']) === 'PUT') { 
         parse_str(file_get_contents("php://input"), $_PUT);
-        $_PUT= json_decode(file_get_contents('php://input',true));
+        //$_PUT= json_decode(file_get_contents('php://input',true));
 
          // para subir el archivo de imagen desde la PC
          if (isset($_FILES['img_url']) && $_FILES['img_url']['error'] === UPLOAD_ERR_OK) {
