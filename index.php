b5a0574981eb0806eccde71c4c887ceff61f7564
rik <ingeniero_rik@hotmail.com>
2024-07-02 22:11:30 -0300

    modif index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 // para la autenticacion del administrador del front. en .env esta la clave
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'auth') {
-    include 'src/utils/auth.php';
+    include './src/utils/auth.php';
     exit;
 }
 include './src/controller/peliculasController.php';
